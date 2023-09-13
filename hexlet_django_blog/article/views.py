2f7fb4a5872e4c3e07d1f1a26a016684bfc505f3
Andrey Bogatyrev <t2way@yandex.ru>
2023-09-13 18:22:47 +0300

    add templates/articles/article.html & correct article/views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'article.html')
+    return render(request, 'articles/article.html', context = {'title': 'Article'})
