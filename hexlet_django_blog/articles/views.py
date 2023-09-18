e413c02d4abd9998fef324bf8bc5a400d3620e01
Andrey Bogatyrev <t2way@yandex.ru>
2023-09-19 00:29:21 +0300

    update articles/views.py: use import View & class IndexView(View) so update articles/urls.py: import IndexView & use it in path as_view()

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-def index(request):
-    return render(request, 'articles/article.html', context = {'title': 'Article'})
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles/article.html', context = {'title': 'Article'})
