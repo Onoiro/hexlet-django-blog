2800be899b38cf413408bbbb90fcaaff976a2cc7
Andrey Bogatyrev <t2way@yandex.ru>
2023-09-23 14:15:08 +0300

    update aricles/urls.py & articles/views.py: trying to use reverse() & redirect from articles.index to dinamic path('<str:tags>/<int:article_id>/', article_view, name='article')

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, article_view
 
 
 urlpatterns = [
-    path('', IndexView.as_view())
+    path('', IndexView.as_view()),
+    path('<str:tags>/<int:article_id>/', article_view, name='article')
 ]
--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'articles/article.html', context = {'title': 'Article'})
+        # return render(request, 'articles/article.html', context = {'title': 'Article'})
+        return redirect('article', kwargs={'article_id': 42, 'tags': 'python'}) 
+
+
+def article_view(request):
+    return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
