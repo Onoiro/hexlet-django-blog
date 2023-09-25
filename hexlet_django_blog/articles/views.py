f9fb8b3d617dc21b56ba1e05d60fe584e3d6b3f2
Andrey Bogatyrev <t2way@yandex.ru>
2023-09-26 00:59:38 +0300

    update articles/urls.py & articles/views.py: add class ArticleView with get() so update urls.py with import ArticleView & correct it's path

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -3,9 +3,14 @@ from django.views import View
 from django.http import HttpResponse
 from django.urls import reverse
 
+
 class IndexView(View):
+    
     def get(self, request, *args, **kwargs):
         return redirect(reverse('article', args=['python', 42]))
 
-    def article(self, request, tags, article_id):
+
+class ArticleView(View):
+    
+    def get(self, request, tags, article_id):
         return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
