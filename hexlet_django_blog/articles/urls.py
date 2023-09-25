a055706bc0ded7d040470d0a629c7ccee8543985
Andrey Bogatyrev <t2way@yandex.ru>
2023-09-26 01:15:46 +0300

    correct articles/urls.py & articles/views.py for linter check

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -4,5 +4,7 @@ from .views import IndexView, ArticleView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<str:tags>/<int:article_id>/', ArticleView.as_view(), name='article'),
+    path('<str:tags>/<int:article_id>/',
+         ArticleView.as_view(),
+         name='article'),
 ]
--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 
 
 class IndexView(View):
-    
+
     def get(self, request, *args, **kwargs):
         return redirect(reverse('article', args=['python', 42]))
 
 
 class ArticleView(View):
-    
+
     def get(self, request, tags, article_id):
         return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
