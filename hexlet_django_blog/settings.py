54e2723afb097132789065e7e9a7d17113301cc3
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-02 17:01:46 +0300

    update settings.py: import load_dotenv(), move SECRET_KEY & DEBUG to .env

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9&_^20bp(ysuabv)a7#6m!c0=(3h&^vfpv(saebags%fg62o&7'
+# SECRET_KEY = 'django-insecure-9&_^20bp(ysuabv)a7#6m!c0=(3h&^vfpv(saebags%fg62o&7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
