72ecffaa8d12835f065615de3f6e1ac00804afad
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-03 16:05:05 +0300

    - register Article in admin.py - correct models.py - update articles/urls.py & articles/views.py to set articles/index.html - move articles/article.html to articles/index.html

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import IndexView, ArticleView
+from .views import IndexView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<str:tags>/<int:article_id>/',
-         ArticleView.as_view(),
-         name='article'),
 ]
--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.views import View
-from django.http import HttpResponse
 from django.urls import reverse
+from .models import Article
+# from django.http import HttpResponse
+
 
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        return redirect(reverse('article', args=['python', 42]))
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html',
+                              context={'articles': articles})
 
 
-class ArticleView(View):
+# class ArticleView(View):
 
-    def get(self, request, tags, article_id):
-        return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+#     def get(self, request, tags, article_id):
+#         return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
