c64ed10ae03e3e32a57394e211cac9d4c81e39fa
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-04 16:12:18 +0300

    correct urls.py: remove import & path to ArticleCommentsView update article/index.html: add href from each article to it's page show.html

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import IndexView, ArticleView, ArticleCommentsView
+from .views import IndexView, ArticleView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:id>/', ArticleView.as_view(), name='article_view'),
-    path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view())
 ]
