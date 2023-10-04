3d86473e642f287ab79fb80d58746e557002d993
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-04 15:53:41 +0300

    update articles/urls.py: add import & path to ArticleCommentsView

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, ArticleView
+from .views import IndexView, ArticleView, ArticleCommentsView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<int:id>/', ArticleView.as_view(), name='article_view')
+    path('<int:id>/', ArticleView.as_view(), name='article_view'),
+    path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view())
 ]
