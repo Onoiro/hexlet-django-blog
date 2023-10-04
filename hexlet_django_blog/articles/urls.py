46ee59b451a6c539bdfc2802b66d5a11145c9e53
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-04 15:39:09 +0300

    update articles/urls.py: add import & path to ArticleView

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ArticleView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<int:id>/', ArticleView.as_view(), name='article_view')
 ]
