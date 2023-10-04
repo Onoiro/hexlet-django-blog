a03baf792da5d39b22eceb689f33a33e4aec0ee9
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-04 15:45:28 +0300

    update articles/views.py: add get view for ArticleView

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.urls import reverse
 from .models import Article
@@ -11,10 +11,12 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()[:15]
         return render(request, 'articles/index.html',
-                              context={'articles': articles})
+                      context={'articles': articles})
 
 
-# class ArticleView(View):
+class ArticleView(View):
 
-#     def get(self, request, tags, article_id):
-#         return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html',
+                      context={'article': article})
