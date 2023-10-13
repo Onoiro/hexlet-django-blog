16383c275b7d0170f8bd8b0797e5542ca74461b3
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-13 22:01:14 +0300

    update articles/views.py, articles/create.html, base.html - update class ArticleForm(ModelForm): add def clean_...() for validation 'name'&'body'" - update create.html: add extends from base.html - update base.html: add block messages

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.forms import ModelForm
+from django.contrib import messages
 from .models import Article
 
 
@@ -24,6 +26,22 @@ class ArticleForm(ModelForm):
     class Meta:
         model = Article
         fields = ['name', 'body']
+    
+    def clean_name(self):
+        data = self.cleaned_data['name']
+        if len(data) < 2:
+            self.add_error('name', 'Title should be no less than 2 characters')
+        if len(data) > 20:
+            self.add_error('name', 'Title should be no longer 20 characters')
+        return data
+    
+    def clean_body(self):
+        data = self.cleaned_data['body']
+        if len(data) < 2:
+            self.add_error('body', 'Article should be no less than 2 characters')
+        if len(data) > 2000:
+            self.add_error('body', 'Article should be no longer 2000 characters')
+        return data
 
 
 class ArticleFormCreateView(View):
@@ -36,5 +54,7 @@ class ArticleFormCreateView(View):
         form = ArticleForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Article was added successfully')
             return redirect('index')
+        messages.error(request, 'Error when adding an article')
         return render(request, 'articles/create.html', {'form': form})
