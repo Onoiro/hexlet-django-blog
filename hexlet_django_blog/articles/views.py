80d584d58f4191f4f9fb101e6ed00867f2801a33
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-16 16:45:08 +0300

    add articles/update.html & add post to ArticleFormEditView

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -68,3 +68,14 @@ class ArticleFormEditView(View):
         form = ArticleForm(instance=article)
         return render(request, 'articles/update.html',
                      {'form': form, 'article_id': article_id})
+    
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+        return render(request, 'articles/update.html',
+                      {'form': form, 'article_id': article_id})
