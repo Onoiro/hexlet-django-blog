8be76b72b5821f22ce957f3ce22df79d1ebfc27f
Andrey Bogatyrev <t2way@yandex.ru>
2023-10-18 01:31:39 +0300

    update articles/ urls & views, templates/articles index & show - add path to ArticleFormDeleteView in articles/urls.py - add class ArticleFormDeleteView(View) in articles/views.py - update articles/show.html: add form with delete article - and it's works - update articles/index.html: add form with delete article, but it's not delete this article!!! it's delete first article in list!!! not

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from .views import IndexView, ArticleView, \
-                   ArticleFormCreateView, ArticleFormEditView
+                   ArticleFormCreateView, \
+                   ArticleFormEditView, \
+                   ArticleFormDeleteView
 
 
 urlpatterns = [
-    path('', IndexView.as_view(),
-         name='index'),
-    path('<int:id>/edit/', ArticleFormEditView.as_view(),
-         name='articles_update'),
-    path('<int:id>/', ArticleView.as_view(),
-         name='article_view'),
-    path('create/', ArticleFormCreateView.as_view(),
-         name='articles_create'),
+     path('', IndexView.as_view(),
+          name='index'),
+     path('<int:id>/edit/', ArticleFormEditView.as_view(),
+          name='articles_update'),
+     path('<int:id>/delete/', ArticleFormDeleteView.as_view(),
+          name='articles_delete'),
+     path('<int:id>/', ArticleView.as_view(),
+          name='article_view'),
+     path('create/', ArticleFormCreateView.as_view(),
+          name='articles_create'),
 ]
